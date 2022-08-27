d23711223de13a4d3107ccad62c3d4e781a476a8
Sönke Müller-Lund <soenke@mueller-lund.de>
2022-08-27 15:05:32 +0200

    mls-fractal-music - Controller separated. - Property used.

--- src/main/java/de/muellerlund/ms/fractalmusic/controller/MainController.java
+++ src/main/java/de/muellerlund/ms/fractalmusic/controller/MainController.java
@@ -15,15 +15,25 @@
  *
  */
 
-package de.muellerlund.ms.fractalmusic;
+package de.muellerlund.ms.fractalmusic.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class MainApplication {
+@RestController
+public class MainController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+    @Value("${mls.lostandfound.property}")
+    private String property;
+
+    @RequestMapping("/hw/")
+    static String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hw2/")
+    String home2() {
+        return property;
     }
 }
